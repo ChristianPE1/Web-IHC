190acb2f80243977ab7a6dbb0a28a7d0ec9d26fa
Sergio <sergio@limasoft.com>
2024-12-09 16:45:23 -0500

    agregando 2 anexos

--- components/paintvr/drag-cards.tsx
+++ components/paintvr/drag-cards.tsx
@@ -24,87 +24,27 @@ const Cards = () => {
     <div className="absolute inset-0 z-10" ref={containerRef}>
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1635373670332-43ea883bb081?q=80&w=2781&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Example image"
+        src="/images/Sketching.jpg"
+        alt="SKETCHING CREACION DE LA IDEA"
         rotate="6deg"
         top="20%"
         left="25%"
         className="w-36 md:w-56"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc."
-        buttonText="Ver más"
-        buttonUrl="https://www.google.com"
+        description="Se bocetaron algunas vistas que el usuario iba a tener en el juego, asi como las mecanicas que este iba a tener y posibles interacciones con el usuario."
+        buttonText="Ver Sketching"
+        buttonUrl="https://drive.google.com/file/d/1qpL8otT3f02zOWCEvkNcCqSbnPajZW1l/view"
       />
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1576174464184-fb78fe882bfd?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Example image"
+        src="/images/evaluacionUsuario.jpg"
+        alt="Evaluación con usuarios"
         rotate="12deg"
         top="45%"
         left="60%"
-        className="w-24 md:w-48"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc."
-        buttonText="Ver más"
-        buttonUrl="https://www.google.com"
-      />
-      <Card
-        containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1503751071777-d2918b21bbd9?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Example image"
-        rotate="-6deg"
-        top="20%"
-        left="40%"
-        className="w-52 md:w-80"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc."
-        buttonText="Ver más"
-        buttonUrl="https://www.google.com"
-      />
-      <Card
-        containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?q=80&w=2609&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Example image"
-        rotate="8deg"
-        top="50%"
-        left="40%"
-        className="w-48 md:w-72"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc."
-        buttonText="Ver más"
-        buttonUrl="https://www.google.com"
-      />
-      <Card
-        containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1602212096437-d0af1ce0553e?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Example image"
-        rotate="18deg"
-        top="20%"
-        left="65%"
-        className="w-40 md:w-64"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc."
-        buttonText="Ver más"
-        buttonUrl="https://www.google.com"
-      />
-      <Card
-        containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1622313762347-3c09fe5f2719?q=80&w=2640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Example image"
-        rotate="-3deg"
-        top="35%"
-        left="55%"
-        className="w-24 md:w-48"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc."
-        buttonText="Ver más"
-        buttonUrl="https://www.google.com"
-      />
-       <Card
-        containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1622313762347-3c09fe5f2719?q=80&w=2640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Example image"
-        rotate="-3deg"
-        top="20%"
-        left="15%"
-        className="w-24 md:w-48"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc."
-        buttonText="Ver más"
-        buttonUrl="https://www.google.com"
+        className="w-[350px]"
+        description="Se hizo una evaluacion con diferentes usuarios, de modo que, pudimos observar como ellos interactuan con el ambiente, algunas dificultades que tienen y que tareas se podrian modificar ."
+        buttonText="Ir a los videos"
+        buttonUrl="https://www.youtube.com/playlist?list=PLCFYbu8kDdyh8CSxQQCByQCPL1rVQVTpQ"
       />
     </div>
   );
@@ -218,7 +158,7 @@ const Card: React.FC<CardProps> = ({
          </button>
      
          {/* Imagen a la izquierda */}
-         <div className="w-1/2 flex justify-center">
+         <div className="flex justify-center">
            <img
              src={src}
              alt={alt}
@@ -228,7 +168,7 @@ const Card: React.FC<CardProps> = ({
      
          {/* Descripción y botón a la derecha */}
          <div className="w-1/2 pl-6">
-           <h2 className="text-xl font-bold mb-4">{alt}</h2>
+           <h2 className="text-xl font-bold mb-4 text-black">{alt}</h2>
            <p className="text-gray-700 mb-6">{description}</p>
            <a
              href={buttonUrl}
