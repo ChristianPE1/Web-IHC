0baeb8efb7578cf160a35e66ead9f69932ce70cf
Sergio <sergio@limasoft.com>
2024-12-09 14:58:09 -0500

    drag card

--- app/(routes)/paintvr/page.tsx
+++ app/(routes)/paintvr/page.tsx
@@ -1,6 +1,7 @@
 import CoverParticles from '@/components/cover-particles';
 import HeroSection from '@/components/paintvr/hero-section';
 import TransitionPage from '@/components/transition-page';
+import DragCards from '@/components/paintvr/drag-cards';
 
 export default function PaintVR() {
    return (
@@ -9,6 +10,7 @@ export default function PaintVR() {
          <CoverParticles colorParticles='#d97706' />
          <HeroSection />
          <div className="fixed left-0 top-0 -z-10 h-full w-full"><div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#d97706_100%)]"></div></div>
+         <DragCards />
       </>
    );
 }
